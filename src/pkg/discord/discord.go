2b74e68d138a91d8f9cd50850167bc2cc897d2da
imalic3 <manovisut.ktp@gmail.com>
2021-07-14 21:23:54 +0700

    fixes discord package

--- src/pkg/discord/discord.go
+++ src/pkg/discord/discord.go
@@ -6,5 +6,5 @@ import (
 )
 
 func GetDiscordContent(swap *model.Swap) string {
-	return fmt.Sprintf(`{"content":"> %s → %s\n*Expected profit*: **%.2f%%**\n%s"}`, swap.InputToken, swap.OutputToken, swap.Changed, swap.URL)
+	return fmt.Sprintf(`{"content":"> %s → %s\n*Expected profit*: **%.2f%%**\n%s"}`, swap.InputToken.Name, swap.OutputToken.Name, swap.Changed, swap.URL)
 }
